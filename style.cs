51ede3e26811b995dae6bdf6347d1a9c565506b6
Senne Berger <sennebergerbc@gmail.com>
2019-07-10 16:58:43 +0200

    derp

---
+++ style.cs
@@ -0,0 +1,40 @@
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+button {
+  margin: 20px;
+}
+
+#field {
+  width: 400px;
+  height: 400px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.cell {
+  width: 38px;
+  height: 38px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid green;
+  background-image: url("grooter.gif");
+}
+
+.bgblack {
+  background-color: black;
+  color: white;
+}
+
+/*# sourceMappingURL=style.cs.map */
